a42457279af4795f58964cfb07b40d2fa5c701ed
HieuBM <buiminhhieusct@gmail.com>
2025-05-20 07:58:22 +0700

    Refactor layout components to include Header in admin and student views; add Header component for teacher view; implement search and notification features in Header components.

--- src/views/admin/Layout.vue
+++ src/views/admin/Layout.vue
@@ -1,11 +1,13 @@
 <template>
-        <LeftSideBar />
-        <div class="md:ml-64 ml-52">
-            <RouterView />
-        </div>
+    <Header />
+    <LeftSideBar />
+    <div class="md:ml-64 ml-52">
+        <RouterView />
+    </div>
 </template>
 <script setup lang="ts">
 
+import Header from './Header.vue';
 import LeftSideBar from './LeftSideBar.vue';
 
 
